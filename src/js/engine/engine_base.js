ClementSparrow/Pattern-Script

commit 7c365ba1f58a8a5c5506daee5a06e8efeee4ae5e

drawMessageScreen implies titleScreen = false

src/js/engine/engine_base.js
@@ -33,14 +33,13 @@ function loadLevelFromLevelDat(state, leveldat, randomseed)
 	loadedLevelSeed = randomseed;
 	RandomGen = new RNG(loadedLevelSeed);
 	forceRegenImages()
-	titleScreen=false;
 	titleMode=(curlevel>0||curlevelTarget!==null)?1:0;
 	titleSelection=(curlevel>0||curlevelTarget!==null)?1:0;
 	titleSelected=false;
 	againing=false;
 	if (leveldat===undefined) {
-		consolePrint("Trying to access a level that doesn't exist.",true);
-	goToTitleScreen();
+		consolePrint("Trying to access a level that doesn't exist.", true)
+		goToTitleScreen();
 		return;
 	}
 	if (leveldat.message===undefined) {
@@ -977,7 +976,6 @@ function restorePreservationState(preservationState) {;
 function showTempMessage() {
 	keybuffer=[];
 	screen_layout.content = textmode_screen
-	titleScreen=false;
 	quittingMessageScreen=false;
 	messageselected=false;
 	tryPlaySimpleSound('showmessage')

src/js/engine/message_screen.js
@@ -8,14 +8,6 @@ const terminal_width = empty_terminal_line.length
 const terminal_height = 13
 
 
-const intro_template = Array.from({
-	3: ' Pattern:Script Terminal ',
-	4: ' v 1.7 ',
-	8: ' insert cartridge ',
-	length:terminal_height
-}, l => (l === undefined) ? doted_terminal_line : centerText(l, doted_terminal_line)
-)
-
 var titleImage = []
 var titleScreen = true
 var titleMode = 0 //1 means there are options
@@ -29,11 +21,19 @@ function generateTitleScreen()
 
 	if (state.levels.length === 0)
 	{
-		titleImage = intro_template
+		titleImage = Array.from(
+			{
+				3: ' Pattern:Script Terminal ',
+				4: ' v 1.7 ',
+				8: ' insert cartridge ',
+				length:terminal_height
+			},
+			l => (l === undefined) ? doted_terminal_line : centerText(l, doted_terminal_line)
+		)
 		return;
 	}
 
-	const title = (state.metadata.title !== undefined) ? state.metadata.title : "Pattern:Script Game";
+	const title = (state.metadata.title !== undefined) ? state.metadata.title : 'Pattern:Script Game';
 
 	const title_bottomline = 3
 	const author_bottomline = 5
@@ -60,7 +60,7 @@ function generateTitleScreen()
 		if (attributionsplit.length > author_bottomline - title_bottomline)
 		{
 			attributionsplit.splice(author_bottomline - title_bottomline)
-			logWarning("Author list too long to fit on screen, truncating to three lines.", undefined, true)
+			logWarning('Author list too long to fit on screen, truncating to three lines.', undefined, true)
 		}
 		titleImage.push(...attributionsplit.map( l => alignTextRight(l, Math.max(l.length - terminal_width, 1)) ))
 		// I prefer them centered:
@@ -127,6 +127,7 @@ function wordwrap(str, width)
 function drawMessageScreen()
 {
 	titleMode = 0
+	titleScreen = false
 	screen_layout.content = textmode_screen
 
 	titleImage = Array(terminal_height).fill(empty_terminal_line)

src/js/inputoutput.js
@@ -340,7 +340,6 @@ TextModeScreen.prototype.checkKey = function(e, inputdir)
 		timer = 0
 		quittingMessageScreen = true
 		tryPlaySimpleSound('closemessage')
-		titleScreen = false
 		drawMessageScreen()
 	}
 	return false;

commit 3265935480bbdc31b35ec8edcfe9b65c96d3e2b5

fix bug introduced by 7c365ba1f58a8a5c5506daee5a06e8efeee4ae5e

src/js/engine/engine_base.js
@@ -43,7 +43,8 @@ function loadLevelFromLevelDat(state, leveldat, randomseed)
 		return;
 	}
 	if (leveldat.message===undefined) {
-		titleMode=0;
+		titleMode = 0
+		titleScreen = false
 		screen_layout.content = level_screen
 		level = leveldat.clone();
 		RebuildLevelArrays();