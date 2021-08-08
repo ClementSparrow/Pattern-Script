437e5caa5032bd7961cd3d505865aacacbd9a120
Clément Sparrow <clement.pillias@free.fr>
2021-08-08 21:59:40 +0200

    [Keep up w/ increpare/PuzzleSript@fe075853fc727e39592a60d0478623f22a137f8e] allow recording of inputs across levels

--- src/js/debug.js
+++ src/js/debug.js
@@ -4,6 +4,7 @@ var canYoutube=false;
 var inputHistory=[];
 var compiledText;
 var IDE=true;
+var recordingStartsFromLevel = 0 // for input recorder
 
 function convertLevelToString()
 {
@@ -59,15 +60,18 @@ function dumpTestCase() {
 	var input = inputHistory.concat([]);
 	var outputDat = convertLevelToString();
 
-	var resultarray = [levelDat,input,outputDat,curlevel,loadedLevelSeed];
+	var resultarray = [levelDat,input,outputDat,recordingStartsFromLevel,loadedLevelSeed]
 	var resultstring = JSON.stringify(resultarray);
 	consolePrint("<br>Recorded play session data (for play session tests - testdata.js):<br><br><br>"+resultstring+"<br><br><br>",true);
 
 }
 
-function clearInputHistory() {
-	if (canDump===true) {
-		inputHistory=[];
+function clearInputHistory()
+{
+	if (canDump === true)
+	{
+		inputHistory=[]
+		recordingStartsFromLevel = curlevel
 	}
 }
 
