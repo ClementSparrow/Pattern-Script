ClementSparrow/Pattern-Script

commit d5cf4095c7c496cafcd592db31af8f16a86e3429

use a BitVec instead of a command queue

src/js/engine/commands_set.js
@@ -1,0 +1,38 @@
+
+function CommandsSet()
+{
+	BitVec.call(this, 13) // todo
+	this.message = null
+	this.nb_commands = 0
+}
+CommandsSet.prototype = Object.create(BitVec.prototype)
+
+// DO NOT CHANGE THE ORDER OF COMMANDS
+CommandsSet.commandwords = [ 'cancel', 'restart', 'again', 'win', 'checkpoint', 'sfx0', 'sfx1', 'sfx2', 'sfx3', 'sfx4', 'sfx5', 'sfx6', 'sfx7', 'sfx8', 'sfx9', 'sfx10', 'message' ]
+CommandsSet.command_keys = {}
+CommandsSet.commandwords.forEach( (word, index) => { CommandsSet.command_keys[word] = index} )
+
+CommandsSet.prototype.is_command = function(word)
+{
+	return CommandsSet.command_keys.hasOwnProperty(word)
+}
+
+CommandsSet.prototype.addCommand = function(command)
+{
+	const key = CommandsSet.command_keys[command]
+	if (this.get(key))
+		return
+	this.ibitset(key)
+	this.nb_commands++
+}
+
+CommandsSet.prototype.setMessage = function(msg_text)
+{
+	this.message = msg_text
+	this.addCommand('message')
+}
+
+CommandsSet.prototype.get_representation = function()
+{
+	return CommandsSet.commandwords.filter( (k,i) => this.get(i) ).join(' ').replace('message', '(message, "'+this.message+'")')
+}

src/js/engine/engine_base.js
@@ -349,8 +349,8 @@ function DoRestart(force) {
 		processInput(-1, true)
 	}
 	
-	level.commandQueue=[];
-	level.commandQueueSourceRules=[];
+	level.commandQueue.setZero()
+	level.commandQueue.sourceRules = []
 	restarting=false;
 }
 
@@ -527,8 +527,8 @@ function commitPreservationState(ruleGroupIndex)
 		rigidGroupIndexMask:level.rigidGroupIndexMask.concat([]),
 		rigidMovementAppliedMask:level.rigidMovementAppliedMask.concat([]),
 		bannedGroup:level.bannedGroup.concat([]),
-		commandQueue:level.commandQueue.concat([]),
-		commandQueueSourceRules:level.commandQueueSourceRules.concat([])
+		commandQueue:level.commandQueue.clone(),
+		commandQueueSourceRules:level.commandQueue.sourceRules.concat([])
 	};
 	rigidBackups[ruleGroupIndex]=propagationState;
 	return propagationState;
@@ -540,8 +540,8 @@ function restorePreservationState(preservationState) {;
 	level.movements = new Int32Array(preservationState.movements);
 	level.rigidGroupIndexMask = preservationState.rigidGroupIndexMask.concat([]);
 	level.rigidMovementAppliedMask = preservationState.rigidMovementAppliedMask.concat([]);
-	level.commandQueue = preservationState.commandQueue.concat([]);
-	level.commandQueueSourceRules = preservationState.commandQueueSourceRules.concat([]);
+	preservationState.commandQueue.cloneInto(level.commandQueue)
+	level.commandQueue.sourceRules = preservationState.commandQueueSourceRules.concat([])
 	sfxCreateMask.setZero();
 	sfxDestroyMask.setZero();
 	consolePrint("Rigid movement application failed, rolling back");
@@ -560,21 +560,18 @@ function showTempMessage()
 
 function processOutputCommands(commands)
 {
-	for (var command of commands)
+	for (var k = CommandsSet.command_keys['sfx0']; k <= CommandsSet.command_keys['sfx10']; k++)
 	{
-		if (command.charAt(1)==='f') //identifies sfxN
+		if (commands.get(k))
 		{
-			tryPlaySimpleSound(command)
-		}  	
-		if (unitTesting === false)
-		{
-			if (command === 'message')
-			{
-				keybuffer = []
-				msg_screen.done = false
-				showTempMessage()
-			}
+			tryPlaySimpleSound(CommandsSet.commandwords[k])
 		}
+	}
+	if ( (unitTesting === false) && (commands.message !== null) )
+	{
+		keybuffer = []
+		msg_screen.done = false
+		showTempMessage()
 	}
 }
 
@@ -769,8 +766,8 @@ function processInput(dir, dontDoWin, dontModify)
 
 		rigidBackups = []
 		level.bannedGroup = []
-		level.commandQueue = []
-		level.commandQueueSourceRules = []
+		level.commandQueue.setZero()
+		level.commandQueue.sourceRules = []
 
 		var i = max_rigid_loops
 		const startState = commitPreservationState()
@@ -821,11 +818,11 @@ function processInput(dir, dontDoWin, dontModify)
 
 
 		// CANCEL command
-		if (level.commandQueue.indexOf('cancel') >= 0)
+		if (level.commandQueue.get(CommandsSet.command_keys.cancel))
 		{
 			if (verbose_logging)
 			{
-				consolePrintFromRule('CANCEL command executed, cancelling turn.', level.commandQueueSourceRules[level.commandQueue.indexOf('cancel')], true)
+				consolePrintFromRule('CANCEL command executed, cancelling turn.', level.commandQueue.sourceRules[CommandsSet.command_keys.cancel], true)
 			}
 			processOutputCommands(level.commandQueue)
 			tryPlaySimpleSound('cancel')
@@ -836,11 +833,11 @@ function processInput(dir, dontDoWin, dontModify)
 		} 
 
 		// RESTART command
-		if (level.commandQueue.indexOf('restart') >= 0)
+		if (level.commandQueue.get(CommandsSet.command_keys.restart))
 		{
 			if (verbose_logging)
 			{
-				consolePrintFromRule('RESTART command executed, reverting to restart state.', level.commandQueueSourceRules[level.commandQueue.indexOf('restart')], true)
+				consolePrintFromRule('RESTART command executed, reverting to restart state.', level.commandQueue.sourceRules[CommandsSet.command_keys.restart], true)
 			}
 			processOutputCommands(level.commandQueue)
 			messagetext = ''
@@ -872,7 +869,7 @@ function processInput(dir, dontDoWin, dontModify)
 
 		if (dontModify)
 		{
-			if (level.commandQueue.indexOf('win') >= 0)
+			if (level.commandQueue.get(CommandsSet.command_keys.win))
 				return true
 
 			if (verbose_logging) { consoleCacheDump() }
@@ -915,11 +912,11 @@ function processInput(dir, dontDoWin, dontModify)
 
 		if ( ! winning )
 		{
-			if (level.commandQueue.indexOf('checkpoint') >= 0)
+			if (level.commandQueue.get(CommandsSet.command_keys.checkpoint))
 			{
 				if (verbose_logging)
 				{ 
-					consolePrintFromRule('CHECKPOINT command executed, saving current state to the restart state.', level.commandQueueSourceRules[level.commandQueue.indexOf('checkpoint')])
+					consolePrintFromRule('CHECKPOINT command executed, saving current state to the restart state.', level.commandQueue.sourceRules[CommandsSet.command_keys.checkpoint])
 				}
 				restartTarget = level4Serialization()
 				hasUsedCheckpoint = true
@@ -930,9 +927,9 @@ function processInput(dir, dontDoWin, dontModify)
 				}
 			}	 
 
-			if ( modified && (level.commandQueue.indexOf('again') >= 0) )
+			if ( modified && level.commandQueue.get(CommandsSet.command_keys.again) )
 			{
-				const r = level.commandQueueSourceRules[level.commandQueue.indexOf('again')]
+				const r = level.commandQueue.sourceRules[CommandsSet.command_keys.again]
 
 				//first have to verify that something's changed
 				var old_verbose_logging = verbose_logging
@@ -953,8 +950,8 @@ function processInput(dir, dontDoWin, dontModify)
 			}
 		}
 
-		level.commandQueue = []
-		level.commandQueueSourceRules = []
+		level.commandQueue.setZero()
+		level.commandQueue.sourceRules = []
 	}
 
 	if (verbose_logging) { consoleCacheDump() }
@@ -968,7 +965,7 @@ function checkWin(dontDoWin = false)
 {
 	dontDoWin |= screen_layout.dontDoWin()
 
-	if (level.commandQueue.indexOf('win') >= 0)
+	if (level.commandQueue.get(CommandsSet.command_keys.win))
 	{
 		if (runrulesonlevelstart_phase)
 		{

src/js/engine/level.js
@@ -14,8 +14,8 @@ function Level(lineNumber, width, height, layerCount, objects)
 	// This is both the initial state of the level (constant) and the current state (mutable).
 	this.objects = objects;
 	// These are game state object (undo history) related to the level but not set or read by this class, and should thus probably be moved out of the class.
-	this.commandQueue = [];
-	this.commandQueueSourceRules = [];
+	this.commandQueue = new CommandsSet()
+	this.commandQueue.sourceRules = []
 }
 
 Level.prototype.clone = function()
@@ -200,8 +200,8 @@ Level.prototype.restore = function(lev)
 	}
 
 	againing=false;
-	this.commandQueue=[];
-	this.commandQueueSourceRules=[];
+	this.commandQueue.setZero()
+	this.commandQueue.sourceRules = []
 }
 
 Level.prototype.delta_index = function(direction)

src/js/engine/rule.js
@@ -390,22 +390,17 @@ Rule.prototype.tryApply = function()
 
 Rule.prototype.queueCommands = function()
 {
-	for(const command of this.commands)
+	level.commandQueue.ior(this.commands)
+	if (this.commands.message !== null)
 	{
-		if (level.commandQueue.indexOf(command[0]) >= 0)
-			continue
-
-		level.commandQueue.push(command[0])
-		level.commandQueueSourceRules.push(this)
-
-		if (verbose_logging)
+		level.commandQueue.message = this.commands.message
+	}
+	if (verbose_logging)
+	{
+		for(const command of CommandsSet.commandwords.filter( (k,i) => this.commands.get(i) ) )
 		{
-			consolePrint('<font color="green">Rule ' + makeLinkToLine(this.lineNumber) + ' triggers command ' + command[0] + '.</font>', true)
-		}
-
-		if (command[0] === 'message')
-		{
-			messagetext = command[1]
+			consolePrint('<font color="green">Rule ' + makeLinkToLine(this.lineNumber) + ' triggers command ' + command + '.</font>', true)
+			level.commandQueue.sourceRules[CommandsSet.command_keys[command]] = this
 		}
 	}
 }

src/js/compiler/rule.js
@@ -84,16 +84,8 @@ function cacheRuleStringRep(identifiers, rule)
 	for (const cellRow of rule.rhs) {
 		result = result + printCellRow(identifiers, cellRow);
 	}
-	for (const command of rule.commands)
-	{
-		if (command.length===1) {
-			result = result + command[0].toString();
-		} else {
-			result = result + '('+command[0].toString()+", "+command[1].toString()+') ';			
-		}
-	}
-	//print commands next
-	rule.stringRep = result;
+	result += rule.commands.get_representation()
+	rule.stringRep = result
 }
 
 function cacheAllRuleNames(state)

src/editor.html
@@ -218,6 +218,7 @@ LEVELS
 <script src="js/engine/message_screen.js"></script>
 <script src="js/engine/level.js"></script>
 <script src="js/engine/bitvec.js"></script>
+<script src="js/engine/commands_set.js"></script>
 <script src="js/engine/rule.js"></script>
 <script src="js/engine/cell_pattern.js "></script>
 <script src="js/engine/engine_base.js"></script>

src/js/buildStandalone.js
@@ -25,8 +25,8 @@ function getLocalFile(relative_url, error_message, success_callback)
 var standalone_JS_Strings = []
 const standalone_JS_Files = [
 	'globalVariables', 'debug_off', 'font', 'rng', 'riffwave', 'editor/random_sound_generators', 'sfxr2', 'codemirror/stringstream', 'colors', 'engine/screen_layout', 'graphics',
-	'engine/log', 'engine/message_screen', 'engine/level', 'engine/bitvec', 'engine/rule', 'engine/cell_pattern', 'engine/engine_base', 'compiler/identifiers', 'compiler/rule',
-	'compiler/rule_parser', 'compiler/rule_expansion', 'compiler/rule_groups', 'parser', 'compiler', 'inputoutput', 'mobile'
+	'engine/log', 'engine/message_screen', 'engine/level', 'engine/bitvec', 'engine/commands_set', 'engine/rule', 'engine/cell_pattern', 'engine/engine_base', 'compiler/identifiers',
+	'compiler/rule', 'compiler/rule_parser', 'compiler/rule_expansion', 'compiler/rule_groups', 'parser', 'compiler', 'inputoutput', 'mobile'
 ]
 
 function record_js_file(i, text, next_step)

src/js/compiler/rule_groups.js
@@ -1,32 +1,33 @@
 
+// TODO: the only role of this function is to convert the format for rules used during compilation into a Rule object, which has almost the same structure.
+// the function could thus probably be removed if we used Rule objects sooner in the pipeline.
 function collapseRules(groups)
 {
 	for (var rules of groups)
 	{
 		for (const [i, oldrule] of rules.entries())
 		{
-			var newrule = [0, [], oldrule.rhs.length>0, oldrule.lineNumber/*ellipses,group number,rigid,commands,randomrule,[cellrowmasks]*/];
-			var ellipses = Array(oldrule.lhs.length).fill(false);
+			var newrule = [0, [], oldrule.rhs.length>0, oldrule.lineNumber/*ellipses,group number,rigid,commands,randomrule,[cellrowmasks]*/]
+			var ellipses = Array(oldrule.lhs.length).fill(false)
 
-			newrule[0] = dirMasks[oldrule.direction];
-			for (var j=0; j<oldrule.lhs.length; j++)
+			newrule[0] = dirMasks[oldrule.direction]
+			newrule[1] = Array.from(oldrule.lhs)
+			for (const [j, cellrow_l] of oldrule.lhs.entries())
 			{
-				var cellrow_l = oldrule.lhs[j];
 				for (const cell of cellrow_l)
 				{
 					if (cell === ellipsisPattern)
 					{
 						if (ellipses[j])
 						{
-							logError("You can't use two ellipses in a single cell match pattern.  If you really want to, please implement it yourself and send me a patch :) ", oldrule.lineNumber);
+							logError("You can't use two ellipses in a single cell match pattern.  If you really want to, please implement it yourself and send me a patch :) ", oldrule.lineNumber)
 						} 
-						ellipses[j] = true;
+						ellipses[j] = true
 					}
 				}
-				newrule[1][j] = cellrow_l;
 			}
 			newrule.push(ellipses, oldrule.groupNumber, oldrule.rigid, oldrule.commands, oldrule.randomRule, cellRowMasks(newrule), oldrule.parameter_expansion_string);
-			rules[i] = new Rule(newrule);
+			rules[i] = new Rule(newrule)
 		}
 	}
 	matchCache = {}; // clear match cache so we don't slowly leak memory
@@ -39,7 +40,7 @@ function ruleGroupRandomnessTest(ruleGroup)
 	const firstLineNumber = ruleGroup[0].lineNumber;
 	for (var i=1;i<ruleGroup.length;i++)
 	{
-		var rule=ruleGroup[i];
+		const rule = ruleGroup[i]
 		if (rule.lineNumber === firstLineNumber) // random [A | B] gets turned into 4 rules, skip
 			continue;
 		if (rule.randomRule)
@@ -49,6 +50,7 @@ function ruleGroupRandomnessTest(ruleGroup)
 	}
 }
 
+// remove from a group the rules that have a 'discard' field
 function ruleGroupDiscardOverlappingTest(ruleGroup)
 {
 	var firstLineNumber = ruleGroup[0].lineNumber;
@@ -56,12 +58,12 @@ function ruleGroupDiscardOverlappingTest(ruleGroup)
 	var example = null;
 	for (var i=0; i<ruleGroup.length; i++)
 	{
-		var rule = ruleGroup[i];
+		const rule = ruleGroup[i]
 		if (rule.hasOwnProperty('discard'))
 		{
-			example = rule['discard'];
-			ruleGroup.splice(i,1);
-			i--;
+			example = rule['discard']
+			ruleGroup.splice(i, 1)
+			i--
 		} else {
 			allbad = false;
 		}
@@ -99,9 +101,9 @@ function arrangeRulesByGroupNumber(state)
 	{
 		var targetArray = rule.late ? aggregates_late : aggregates;
 
-		if (targetArray[rule.groupNumber] == undefined)
+		if (targetArray[rule.groupNumber] === undefined)
 		{
-			targetArray[rule.groupNumber] = [];
+			targetArray[rule.groupNumber] = []
 		}
 		targetArray[rule.groupNumber].push(rule);
 	}

commit 4d960ded6a090617a424f7be99c2bc7550f9fb42

Fix a bug introduced by d5cf4095c7c496cafcd592db31af8f16a86e3429

src/js/engine/commands_set.js
@@ -37,6 +37,7 @@ CommandsSet.prototype.reset = function()
 	this.setZero()
 	this.message = null
 }
+
 CommandsSet.prototype.get_representation = function()
 {
 	return CommandsSet.commandwords.filter( (k,i) => this.get(i) ).join(' ').replace('message', '(message, "'+this.message+'")')

src/js/engine/engine_base.js
@@ -349,7 +349,7 @@ function DoRestart(force) {
 		processInput(-1, true)
 	}
 	
-	level.commandQueue.setZero()
+	level.commandQueue.reset()
 	level.commandQueue.sourceRules = []
 	restarting=false;
 }
@@ -766,7 +766,7 @@ function processInput(dir, dontDoWin, dontModify)
 
 		rigidBackups = []
 		level.bannedGroup = []
-		level.commandQueue.setZero()
+		level.commandQueue.reset()
 		level.commandQueue.sourceRules = []
 
 		var i = max_rigid_loops
@@ -948,7 +948,7 @@ function processInput(dir, dontDoWin, dontModify)
 			}
 		}
 
-		level.commandQueue.setZero()
+		level.commandQueue.reset()
 		level.commandQueue.sourceRules = []
 	}
 

src/js/engine/level.js
@@ -200,7 +200,7 @@ Level.prototype.restore = function(lev)
 	}
 
 	againing=false;
-	this.commandQueue.setZero()
+	this.commandQueue.reset()
 	this.commandQueue.sourceRules = []
 }
 

src/js/engine/rule.js
@@ -416,7 +416,7 @@ Rule.prototype.queueCommands = function()
 
 	if (this.commands.message !== null)
 	{
-		level.commandQueue.message = this.commands.message
+		messagetext = level.commandQueue.message = this.commands.message
 	}
 
 	if (verbose_logging)