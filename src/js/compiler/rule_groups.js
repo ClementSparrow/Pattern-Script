ClementSparrow/Pattern-Script

commit 5b8d35c283bfc22257bada8e8bc42cb51f75b9d7

Allow compilation messages to change without breaking unit tests
- replaced most calls to logError and logWarning with message identifiers defined in error_messages.js.
- simplified the error-login functions in debug.js
- now log separately errors and warnings
- updated unit tests and dumping (Ctrl+J) to work with the new representation of compilation messages
- added a "copy test data" button to copy the game used in the test
- improved the rendering of unit tests
- unit test are now categorized
- added a category of unit tests that fetches directly the demo files to check they do not produce compilation messages, and removed the corresponding tests from errormessage_testdata.js
- removed some compilation messages that systematically doubled another one
- in addition to making it easier to update the compilation messages, all this also make it easier to translate the messages!
- never again

src/js/compiler/rule_groups.js
@@ -20,7 +20,7 @@ function collapseRules(groups)
 					{
 						if (ellipses[j])
 						{
-							logError("You can't use two ellipses in a single cell match pattern.  If you really want to, please implement it yourself and send me a patch :) ", oldrule.lineNumber)
+							logError(['more_than_one_ellipses_in_cellrow'], oldrule.lineNumber)
 						} 
 						ellipses[j] = true
 					}
@@ -36,6 +36,7 @@ function collapseRules(groups)
 // remove from a group the rules that have a 'discard' field. We could display the error message as soon as ruleToMask, but we want to factorize the same error messages
 // created by different expansions of a same original rule. Normally this is done by the console's deletion of repeated messages, but here the example can be different
 // for the different expansions.
+// See https://github.com/increpare/PuzzleScript/issues/512 and https://github.com/increpare/PuzzleScript/issues/605
 function ruleGroupDiscardOverlappingTest(ruleGroup)
 {
 	var firstLineNumber = ruleGroup[0].lineNumber;
@@ -55,7 +56,7 @@ function ruleGroupDiscardOverlappingTest(ruleGroup)
 	}
 	if (allbad)
 	{
-		logError(example[0] +' and '+example[1]+' can never overlap, but this rule requires that to happen.', firstLineNumber);
+		logError(['overlapping_objects_in_cell', ...example], firstLineNumber)
 	}
 }
 
@@ -192,7 +193,7 @@ function generateLoopPointsAux(loops, rules)
 		}
 		if (loop[1] === (outside ? -1 : 1) )
 		{
-			logErrorNoLine("Need to have matching number of 'startLoop' and 'endLoop' loop points.");
+			logError(['unbalanced_loop'])
 		}
 		outside = ! outside
 	}
@@ -204,7 +205,7 @@ function generateLoopPoints(state)
 {
 	if (state.loops.length % 2 === 1)
 	{
-		logErrorNoLine("have to have matching number of  'startLoop' and 'endLoop' loop points.");
+		logError("have to have matching number of  'startLoop' and 'endLoop' loop points.")
 	}
 	state.loopPoint = generateLoopPointsAux(state.loops, state.rules)
 	state.lateLoopPoint = generateLoopPointsAux(state.loops, state.lateRules)

src/editor.html
@@ -189,6 +189,7 @@ LEVELS
 <script src="js/jsgif/NeuQuant.js"></script>
 <script src="js/jsgif/GIFEncoder.js"></script>
 
+<script src="js/editor/error_messages.js"></script>
 <script src="js/debug.js"></script>
 <script src="js/globalVariables.js"></script>
 <script src="js/font.js"></script>

src/js/compiler.js
@@ -49,7 +49,7 @@ function generateExtraMembers(state)
 
 	if (state.collisionLayers.length === 0)
 	{
-		logError("No collision layers defined.  All objects need to be in collision layers.");
+		logError(['no_collision_layers'])
 	}
 
 	//annotate objects with layers
@@ -97,7 +97,7 @@ function generateExtraMembers(state)
 		}
 		if (colorPalettes[val] === undefined)
 		{
-			logError('Palette "'+val+'" not found, defaulting to arnecolors.',0);
+			logError(['palette_not_found', val]) // TODO: use the line number of the palette declaration
 		} else {
 			colorPalette = colorPalettes[val];
 		}
@@ -112,7 +112,7 @@ function generateExtraMembers(state)
 	for (var o of state.identifiers.objects)
 	{
 		if (o.colors.length>10) {
-			logError("a sprite cannot have more than 10 colors.  Why you would want more than 10 is beyond me.", state.identifiers.lineNumbers[o.identifier_index]+1); // TODO: Seriously??? Just remind that the bitmap definition uses digits for colors, which limits them to ten -- ClementSparrow
+			logError(['too_many_sprite_colors'], state.identifiers.lineNumbers[o.identifier_index]+1)
 		}
 		for (var i=0; i<o.colors.length; i++)
 		{
@@ -122,7 +122,7 @@ function generateExtraMembers(state)
 				c = colorToHex(colorPalette,c);
 				o.colors[i] = c;
 			} else {
-				logError('Invalid color specified for object "' + o.name + '", namely "' + c + '".', state.identifiers.lineNumbers[o.identifier_index] + 1);
+				logError(['invalid_color_for_object', o.name, c], state.identifiers.lineNumbers[o.identifier_index] + 1)
 				o.colors[i] = '#ff00ffff'; // magenta error color
 			}
 		}
@@ -135,7 +135,7 @@ function generateExtraMembers(state)
 		if (o.colors.length == 0)
 		{
 			// TODO: We may want to silently use transparency in that case, considering how frequent it is to use transparent markers in PuzzleScript...
-			logError('color not specified for object "' + o.name +'".', state.identifiers.lineNumbers[o.identifier_index]);
+			logError(['no_palette_in_object', o.name], state.identifiers.lineNumbers[o.identifier_index])
 			o.colors=["#ff00ffff"];
 		}
 		if (o.spritematrix.length === 0)
@@ -144,10 +144,6 @@ function generateExtraMembers(state)
 		}
 		else
 		{
-			if ( o.spritematrix.length !== sprite_height || o.spritematrix.some( line => (line.length !== sprite_width) ) )
-			{
-				logWarning('Sprite graphics must be ' + sprite_width + ' wide and ' + sprite_height + ' high exactly.', state.identifiers.lineNumbers[o.identifier_index]);
-			}
 			o.spritematrix = generateSpriteMatrix(o.spritematrix);
 		}
 	}
@@ -182,19 +178,19 @@ function generateExtraMembers(state)
 
 	if ( (state.idDict[0] === undefined) && (state.collisionLayers.length > 0))
 	{
-		logError('You need to have some objects defined');
+		logError(['no_object'])
 	}
 
 	//set default background object
 	const background_identifier_index = state.identifiers.names.indexOf('background');
 	if (background_identifier_index < 0)
 	{
-		logError("you have to define something to be the background");
+		logError(['no_background'])
 		state.background_index = state.idDict[0];
 	}
 	else if ( ! [identifier_type_object, identifier_type_property].includes(state.identifiers.comptype[background_identifier_index]) )
 	{
-		logError("background cannot be an aggregate (declared with 'and'), it has to be a simple type, or property (declared in terms of others using 'or').");
+		logError(['background_is_aggregate'])
 		state.background_index = state.idDict[0];
 	}
 	else
@@ -211,11 +207,6 @@ function generateExtraMembers(state)
 
 Level.prototype.calcBackgroundMask = function(state)
 {
-	if (state.backgroundlayer === undefined)
-	{
-		logError("you have to have a background layer");
-	}
-
 	var backgroundMask = state.layerMasks[state.backgroundlayer];
 	for (var i=0; i<this.n_tiles; i++)
 	{
@@ -265,18 +256,18 @@ function levelFromString(state, level)
 			const identifier_index = state.identifiers.names.indexOf(ch); // TODO: this should be done in the parser
 			if (identifier_index < 0)
 			{
-				logError('Error, symbol "' + ch + '", used in map, not found.', level[0]+j);
-				continue;
+				logError(['unknown_symbol_in_level', ch], level[0]+j)
+				continue
 			}
 			if (state.identifiers.comptype[identifier_index] == identifier_type_property)
 			{
-				logError('Error, symbol "' + ch + '" is defined using \'or\', and therefore ambiguous - it cannot be used in a map. Did you mean to define it in terms of \'and\'?', level[0]+j);
-				continue;
+				logError(['property_symbol_in_level', ch], level[0]+j)
+				continue
 			}
 			if ( ! [identifier_type_object, identifier_type_aggregate].includes(state.identifiers.comptype[identifier_index]) )
 			{
-				logError('Error, symbol "' + ch + '" is defined as '+identifier_type_as_text[state.identifiers.comptype[identifier_index]]+'. It cannot be used in a map.', level[0]+j);
-				continue;
+				logError(['wrong_symbol_type_in_level', ch, state.identifiers.comptype[identifier_index]], level[0]+j)
+				continue
 			}
 
 			const maskint = makeMaskFromGlyph( state.glyphDict[identifier_index].concat([]) );
@@ -412,7 +403,7 @@ function ruleToMask(state, rule, layerTemplate, layerCount)
 						if (rhscell.length !==1 || rhscell[0] !== null)
 						{
 							// TODO: this should be catched earlier in the compilation pipeline
-							logError("An ellipsis on the left must be matched by one in the corresponding place on the right.", rule.lineNumber);								
+							logError(['no_matching_ellipsis_in_RHS'], rule.lineNumber)
 						}
 					} 
 					break;
@@ -430,7 +421,7 @@ function ruleToMask(state, rule, layerTemplate, layerCount)
 				}
 				else if ((layerIndex === undefined) || (layerIndex < 0))
 				{
-					logError("Oops!  " +state.identifiers.names[oc.ii].toUpperCase()+" not assigned to a layer.", rule.lineNumber);
+					logError(['no_layer_for_object', state.identifiers.names[oc.ii]], rule.lineNumber)
 				}
 				else
 				{
@@ -465,7 +456,7 @@ function ruleToMask(state, rule, layerTemplate, layerCount)
 
 			if ( (rule.rhs.length > 0) && (cellrow_r[k][0] === null) && (cell_l[0] !== null) )
 			{
-				logError("An ellipsis on the right must be matched by one in the corresponding place on the left.", rule.lineNumber);
+				logError(['no_matching_ellipsis_in_LHS'], rule.lineNumber)
 			}
 
 			if (objectsPresent === ellipsisPattern)
@@ -525,7 +516,7 @@ function ruleToMask(state, rule, layerTemplate, layerCount)
 					else
 					{
 						// TODO: this error should be catched earlier in the parsing piepline
-						logError('You want to spawn a random "'+state.identifiers.names[oc.ii].toUpperCase()+'", but I don\'t know how to do that', rule.lineNumber);
+						logError(['spawn_aggregate', state.identifiers.names[oc.ii]], rule.lineNumber)
 					}
 					continue;
 				}
@@ -539,7 +530,7 @@ function ruleToMask(state, rule, layerTemplate, layerCount)
 				}
 				else if ( (layerIndex === undefined) || (layerIndex < 0) )
 				{
-					logError("Oops!  " +state.identifiers.names[oc.ii].toUpperCase()+" not assigned to a layer.", rule.lineNumber);
+					logError(['no_layer_for_object', state.identifiers.names[oc.ii]], rule.lineNumber)
 				}
 				else
 				{
@@ -547,7 +538,7 @@ function ruleToMask(state, rule, layerTemplate, layerCount)
 
 					if ( (existing_index !== null) && ( ! rule.hasOwnProperty('discard') ) ) // "discard" here is used just to not show an error message when we know there will already be one for that rule?
 					{
-						logError('Rule matches object types that can\'t overlap: "' + state.identifiers.names[oc.ii].toUpperCase() + '" and "' + state.identifiers.names[existing_index].toUpperCase() + '".',rule.lineNumber);
+						logError(['cant_overlap', state.identifiers.names[oc.ii], state.identifiers.names[existing_index]], rule.lineNumber)
 					}
 
 					layersUsed_r[layerIndex] = oc.ii;
@@ -646,7 +637,7 @@ function generateMasks(state)
 	const player_identifier_index = state.identifiers.names.indexOf('player');
 	if (player_identifier_index < 0)
 	{
-		logErrorNoLine("error, didn't find any object called player, either in the objects section, or the legends section. there must be a player!");
+		logError(['no_player_defined'])
 		state.playerMask = new BitVec(STRIDE_OBJ);
 	}
 	else
@@ -669,16 +660,17 @@ function generateMasks(state)
 	state.objectMasks = objectMask;
 }
 
-function checkObjectsAreLayered(identifiers)
-{
-	for (var o of identifiers.objects)
-	{
-		if (o.layer === undefined)
-		{
-			logError('Object "' + o.name.toUpperCase() + '" has been defined, but not assigned to a layer.', identifiers.lineNumbers[o.identifier_index]);
-		}
-	}
-}
+// We don't need to check all objects, only those actualy used in rules, which are already checked in ruleToMask.
+// function checkObjectsAreLayered(identifiers)
+// {
+// 	for (var o of identifiers.objects)
+// 	{
+// 		if (o.layer === undefined)
+// 		{
+// 			logError('Object "' + o.name.toUpperCase() + '" has been defined, but not assigned to a layer.', identifiers.lineNumbers[o.identifier_index]);
+// 		}
+// 	}
+// }
 
 function twiddleMetaData(state)
 {
@@ -706,14 +698,14 @@ function tokenizeWinConditionIdentifier(state, n, lineNumber)
 	const identifier_index = state.identifiers.checkKnownIdentifier(n, false, state);
 	if (identifier_index < 0)
 	{
-		logError('Unknown object name "' + n +'" found in win condition.', lineNumber);
-		return null;
+		logError(['unknown_object_in_wincondition', n], lineNumber)
+		return null
 	}
 	const identifier_comptype = state.identifiers.comptype[identifier_index];
 	if ( (identifier_comptype != identifier_type_property) && (identifier_comptype != identifier_type_object) ) // not a property, not an object
 	{
-		logError('Invalid object name found in win condition: ' + n + 'is ' + identifier_type_as_text[identifier_comptype] + ', but win conditions objects have to be objects or properties (defined using "or", in terms of other properties)', lineNumber);
-		return null;
+		logError(['invalid_object_in_wincondition', n, identifier_type_as_text[identifier_comptype]], lineNumber)
+		return null
 	}
 	return state.objectMasks[identifier_index];
 }
@@ -810,7 +802,7 @@ function generateSoundData(state)
 
 		if (sound.length === 2)
 		{
-			logError('incorrect sound declaration.', lineNumber);
+			logError(['incorrect_sound_declaration'], lineNumber)
 			continue;
 		}
 
@@ -838,7 +830,7 @@ function generateSoundData(state)
 			var directions = sound.slice(2, sound.length-2);
 			if (directions.length > 0 && (verb !== 'move' && verb !== 'cantmove'))
 			{
-				logError('incorrect sound declaration.', lineNumber);
+				logError(['incorrect_sound_declaration'], lineNumber)
 			}
 
 			if (verb === 'action')
@@ -1022,7 +1014,7 @@ function loadFile(str)
 			else
 				state.blankLine()
 
-			if (errorCount > MAX_ERRORS)
+			if (errorStrings.length > MAX_ERRORS)
 			{
 				consolePrint("too many errors, aborting compilation");
 				return;
@@ -1061,7 +1053,7 @@ function loadFile(str)
 	generateRigidGroupList(state);
 
 	processWinConditions(state);
-	checkObjectsAreLayered(state.identifiers);
+	// checkObjectsAreLayered(state.identifiers);
 
 	generateLoopPoints(state);
 
@@ -1110,9 +1102,9 @@ function compile(command, text, randomseed)
 		compiledText=text;
 	}
 
-	errorCount = 0;
 	compiling = true;
-	errorStrings = [];
+	errorStrings = []
+	warningStrings = []
 	consolePrint('=================================');
 	try
 	{
@@ -1122,13 +1114,14 @@ function compile(command, text, randomseed)
 	}
 
 	if (state && state.levels && state.levels.length===0){	
-		logError('No levels found. Add some levels!', undefined, true);
+		logError(['no_level_found'], undefined, true)
 	}
 
-	if (errorCount>MAX_ERRORS)
-		return;
+	if (errorStrings.length > MAX_ERRORS)
+		return
 
-	if (errorCount>0) {
+	if (errorStrings.length > 0)
+	{
 		consoleError('<span class="systemMessage">Errors detected during compilation; the game may not work correctly.</span>');
 	}
 	else {

src/js/compiler/identifiers.js
@@ -197,6 +197,9 @@ Identifiers.prototype.registerNewMapping = function(identifier, original_case, f
  * and notably they don't check the identifiers have been registered.
  */
 
+// TODO: instead of having an 'accepts_mapping' parameter and check all possible values in the end set of the mapping, we should provide as argument the set of parameters
+// that can be replaced. accepts_mapping is only true for checking rules and the first parameter of copy: in sprites definitions. In both cases we should know the expansion params.
+
 Identifiers.prototype.visitTagExpansion = function(identifier, tagged_identifier, accepts_mapping, tag_operation, identifier_operation)
 {
 	const tag_identifier_indexes = tagged_identifier[3];
@@ -330,7 +333,7 @@ Identifiers.prototype.canBeAnObjectName = function(candname, log)
 //	Warn if the name is a keyword
 	if (forbidden_keywords.indexOf(candname) >= 0)
 	{
-		log.logWarning('You named an object "' + candname.toUpperCase() + '", but this is a keyword. Don\'t do that!');
+		log.logWarning(['identifier_name_is_keyword', candname])
 		return [0, candname, []]; // yes, this is only a warning
 	}
 //	Check the tags
@@ -355,11 +358,11 @@ Identifiers.prototype.checkIfNewIdentifierIsValid = function(candname, accept_im
 		const l = this.lineNumbers[identifier_index];
 		if (l == -1)
 		{
-			log.logError('You named an object "'+candname.toUpperCase()+'", but this is a keyword. Don\'t do that!');
+			log.logError(['identifier_name_is_keyword', candname]) // TODO: inconsitancy: it's an error here, but a warning in canBeAnObjectName.
 		}
 		else
 		{
-			log.logError('Object "' + candname.toUpperCase() + '" already defined' + definition_string + ' on ' + makeLinkToLine(l, 'line ' + l.toString()));
+			log.logError(['identifier_already_defined', candname, definition_string, l])
 		}
 		return false;
 	}
@@ -401,7 +404,7 @@ Identifiers.prototype.checkKnownIdentifier = function(identifier, accepts_mappin
 	if (error_code < 0)
 		return error_code - 2;
 
-	const tagged_identifier = [identifier_base, identifier_base, tags.map( ([tag_index,tag_name]) => tag_name ), tags.map( ([tag_index,tag_name]) => tag_index ), log.lineNumber]
+	const tagged_identifier = [identifier_base, identifier_base, tags.map( ([,tag_name]) => tag_name ), tags.map( ([tag_index,]) => tag_index ), log.lineNumber]
 
 //	For all possible combinations of tag values in these tag classes, the corresponding object must have been defined (as an object).
 	if ( ! this.visitTagExpansion(identifier, tagged_identifier, accepts_mapping, this.checkTagMapping.bind(this), this.checkImplicitObjectIdentifier.bind(this, log)) )

src/js/compiler/rule_parser.js
@@ -87,7 +87,7 @@ function parseRuleDirections(state, tokens, lineNumber)
 		} else if (token === 'random') {
 			if (has_plus)
 			{
-				logError('A rule-group can only be marked random by the first rule', lineNumber) // TODO: better explain why in the message.
+				logError(['random_on_nonfirst_group_rule'], lineNumber)
 			}
 			randomRule = true;
 		} else if (simpleAbsoluteDirections.indexOf(token) >= 0) {
@@ -188,7 +188,7 @@ function parseRuleString(rule, state, curRules)
 
 	if (tokens.indexOf('->') == -1)
 	{
-		logError("A rule has to have an arrow in it.  There's no arrow here! Consider reading up about rules - you're clearly doing something weird", lineNumber);
+		logError(['rule_without_arrow'], lineNumber)
 	}
 
 	const [ directions, tag_classes, properties, late, rigid, randomRule, has_plus, nb_tokens_in_rule_directions ] = parseRuleDirections(state, tokens, lineNumber);
@@ -234,7 +234,7 @@ function parseRuleString(rule, state, curRules)
 		{
 			bracketbalance++;
 			if (bracketbalance > 1) {
-				logWarning("Multiple opening brackets without closing brackets.  Something fishy here.  Every '[' has to be closed by a ']', and you can't nest them.", lineNumber);
+				logWarning(['rule_open_open_brackets'], lineNumber)
 			}
 			if (curcell.length > 0) { // TODO: isn't that dupplicating what the bracketbalance test does?
 				logError('Error, malformed cell rule - encountered a "["" before previous bracket was closed', lineNumber);
@@ -243,20 +243,20 @@ function parseRuleString(rule, state, curRules)
 			curcell = [];
 		} else if (reg_directions_only.exec(token)) {
 			if (!incellrow) {
-				logWarning("Invalid syntax. Directions should be placed at the start of a rule.", lineNumber);
+				logWarning(['directions_outside_cellrows'], lineNumber);
 			} else if (curobjcond.no || curobjcond.random) {
 				// TODO: it would be nice to allow "no up crate" to match cells that have either no crate or a crate that does not move up. But it requires changes in the engine.
-				logError('Invalid syntax. The keyword "'+(tokens[i-1]).toUpperCase()+'" must be followed by an object name.', lineNumber)
+				logError(['no_or_random_followed_by_direction', tokens[i-1]], lineNumber)
 			} else if (token === 'random') {
 				if ( ! rhs )
 				{
-					logError('"random" cannot be matched on the left-hand side, it can only appear on the right', rule.lineNumber)
+					logError(['random_in_LHS', token], rule.lineNumber)
 				}
 				curobjcond.random = true
 			} else if (token === 'no') {
 				if (curobjcond.dir !== null)
 				{
-					logError('Syntax error: "NO" cannot follow a direction in a rule.', lineNumber)
+					logError(['direction_NO_object'], lineNumber)
 				}
 				curobjcond.no = true
 			} else if (curobjcond.dir !== null) {
@@ -268,7 +268,7 @@ function parseRuleString(rule, state, curRules)
 				//       And we clearly want the ability to have alternatives, but it's just a shortcut to avoid making multiple rules instead.
 				logError("Error, an item can only have one direction/action at a time, but you're looking for several at once!", lineNumber);
 			} else if ( ! rhs && token === 'randomdir' ) {
-				logError('Error, "RANDOMDIR" cannot appear on the left-hand side of a rule, it can only appear on the right.', lineNumber)
+				logError(['random_in_LHS', token], lineNumber)
 			} else {
 				curobjcond.dir = token
 			}
@@ -281,12 +281,12 @@ function parseRuleString(rule, state, curRules)
 		} else if (token === ']') {
 			bracketbalance--;
 			if (bracketbalance < 0) {
-				logWarning("Multiple closing brackets without corresponding opening brackets.  Something fishy here.  Every '[' has to be closed by a ']', and you can't nest them.", lineNumber);
+				logWarning(['rule_close_close_brackets'], lineNumber)
 			}
 			should_close_cellrow = true; // TODO: should it be "should_close_cellrow = (bracketbalance == 0)"?
 		} else if (token === '->') {
 			if (incellrow) {
-				logError('Encountered an unexpected "->" inside square brackets.  It\'s used to separate states, it has no place inside them >:| .', lineNumber);
+				logError(['rule_arrow_in_cell'], lineNumber)
 			} else if (rhs) {
 				logError('Error, you can only use "->" once in a rule; it\'s used to separate before and after states.', lineNumber);
 			} else {
@@ -317,7 +317,7 @@ function parseRuleString(rule, state, curRules)
 			}
 			if (incellrow)
 			{
-				logError('Commands must appear at the end of the rule, outside the cell rows (square brackety things).', lineNumber);
+				logError(['commands_in_cellrow'], lineNumber)
 			}
 			if (token === 'message')
 			{
@@ -371,7 +371,7 @@ function parseRuleString(rule, state, curRules)
 			// close the current cell
 			if ( cell_contains_ellipses && (curcell.length > 1) )
 			{
-				logError('Ellipses shoud be alone in their own cell, like that: |...|', lineNumber);
+				logError(['ellipses_not_alone'], lineNumber)
 			}
 			curcellrow.push(curcell);
 			curcell = [];
@@ -400,17 +400,16 @@ function parseRuleString(rule, state, curRules)
 
 	// Check the coherence between LHS and RHS
 	if (lhs_cells.length != rhs_cells.length) {
-		if (commands.nb_commands > 0 && rhs_cells.length == 0) {
-			//ok
-		} else {
-			logError('Error, when specifying a rule, the number of matches (square bracketed bits) on the left hand side of the arrow must equal the number on the right', lineNumber);
+		if ( (commands.nb_commands === 0) || (rhs_cells.length > 0) )
+		{
+			logError(['different_nb_cellrows'], lineNumber)
 		}
 	} else {
 		for (const [i, lhs_cell] of lhs_cells.entries())
 		{
 			if (lhs_cell.length != rhs_cells[i].length) {
-				logError('In a rule, each pattern to match on the left must have a corresponding pattern on the right of equal length (number of cells).', lineNumber);
-				return null; // ignoring the rule because it would cause bugs later in the code.
+				logError(['different_nb_cells'], lineNumber)
+				return null // ignoring the rule because it would cause bugs later in the code.
 			}
 		}
 	}

src/js/debug.js
@@ -39,31 +39,27 @@ function convertLevelToString()
 	return out;
 }
 
-function stripHTMLTags(html_str){
+function stripHTMLTags(html_str)
+{
+	if (typeof html_str !== 'string')
+		return html_str
 	var div = document.createElement("div");
 	div.innerHTML = html_str;
 	var text = div.textContent || div.innerText || "";
 	return text.trim();
 }
 
-function dumpTestCase() {
+function dumpTestCase()
+{
 	//compiler error data
-	var levelDat = compiledText;
-	var errorStrings_stripped = errorStrings.map(stripHTMLTags);
-	var resultarray = [levelDat,errorStrings_stripped,errorCount];
-	var resultstring = JSON.stringify(resultarray);
-	consolePrint("<br>Compilation error/warning data (for error message tests - errormessage_testdata.js):<br><br><br>"+resultstring+"<br><br><br>",true);
-
+	const levelDat = compiledText
+	const resultstring = JSON.stringify( [levelDat, errorStrings.map(stripHTMLTags), warningStrings.map(stripHTMLTags)] )
+	consolePrint("<br>Compilation error/warning data (for error message tests - errormessage_testdata.js):<br><br><br>"+resultstring+"<br><br><br>", true)
 	
 	//normal session recording data
 	if (level !== undefined)
 	{
-		var levelDat = compiledText;
-		var input = inputHistory.concat([]);
-		var outputDat = convertLevelToString();
-
-		var resultarray = [levelDat,input,outputDat,recordingStartsFromLevel,loadedLevelSeed]
-		var resultstring = JSON.stringify(resultarray);
+		const resultstring = JSON.stringify( [levelDat, inputHistory.concat([]), convertLevelToString(), recordingStartsFromLevel, loadedLevelSeed] )
 		consolePrint("<br>Recorded play session data (for play session tests - testdata.js):<br><br><br>"+resultstring+"<br><br><br>",true);
 	}
 }

commit 8967ec98165505e034422e89247a425b55129e2f

Fix a broken error message
this bug was introduced with 5b8d35c283bfc22257bada8e8bc42cb51f75b9d7 it does not affect vanilla PuzzleScript

src/js/compiler/rule_groups.js
@@ -56,7 +56,7 @@ function ruleGroupDiscardOverlappingTest(ruleGroup)
 	}
 	if (allbad)
 	{
-		logError(['overlapping_objects_in_cell', ...example], firstLineNumber)
+		logError(['overlapping_objects_in_cell', example], firstLineNumber)
 	}
 }
 