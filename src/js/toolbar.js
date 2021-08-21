ClementSparrow/Pattern-Script

commit 3fee325b53833e9d80fa00c150197101b6f653ad

use a `level` parameter instead of `command` in functions `compile` and `setGameState`
where `level < 0` means restart, `level === undefined` means rebuild, and `level` can be a callback function to pick a level according to some logic. This is used to move editor-related code out of engine_base.js, and to provide more flexibility in the future.

src/js/toolbar.js
@@ -16,7 +16,7 @@ function setPageTitle()
 function runClick()
 {
 	clearConsole()
-	compile(["restart"])
+	compile(-1)
 	setPageTitle()
 }
 
@@ -181,7 +181,7 @@ function levelEditorClick_Fn()
 {
 	// if ( (screen_layout.content === msg_screen) || (state.levels.length === 0) )
 	// {
-	// 	compile(['loadLevel', 0])
+	// 	compile(0)
 	// }
 	level_editor_screen.toggle()
 	// forceRegenImages()
@@ -226,7 +226,7 @@ function shareOnGitHub(is_public, should_fork=false)
 		return;
 	}
 
-	compile(["rebuild"]);
+	compile()
 
 	const title = (state.metadata.title !== undefined) ? state.metadata.title + " ("+PSFORKNAME+" Script)" : ("Untitled "+PSFORKNAME+" Script");
 	const source = editor.getValue();
@@ -336,7 +336,7 @@ function rebuildClick()
 function exportClick()
 {
 	const sourceCode = editor.getValue();
-	compile("restart");
+	compile(-1)
 	buildStandalone(JSON.stringify(sourceCode));
 }
 

src/js/compiler.js
@@ -1069,7 +1069,7 @@ function loadFile(str)
 	return state;
 }
 
-function compile(command = ["restart"], text, randomseed)
+function compile(level, text, randomseed) // level = -1 means restart, level = undefined means rebuild
 {
 	matchCache = {}
 	forceRegenImages()
@@ -1115,10 +1115,10 @@ function compile(command = ["restart"], text, randomseed)
 		for (const rule of state.lateRules) {
 			ruleCount += rule.length
 		}
-		consolePrint('<span class="systemMessage">Successful ' + ((command[0] == 'restart')?'Compilation':'live recompilation') + ', generated '+ruleCount+' instructions.</span>')
+		consolePrint('<span class="systemMessage">Successful ' + ((level === undefined) ? 'live re' : '') + 'compilation, generated '+ruleCount+' instructions.</span>')
 	}
 
-	setGameState(state, command, randomseed)
+	setGameState(state, level, randomseed)
 
 	clearInputHistory()
 

src/js/editor.js
@@ -141,7 +141,18 @@ editor.on('mousedown', function(cm, event)
 			document.activeElement.blur()  // unfocus code panel
 			editor.display.input.blur()
 			prevent(event)         // prevent refocus
-			compile(["levelline", cm.posFromMouse(event).line])
+			const targetLine = cm.posFromMouse(event).line
+			compile(
+				function(levels)
+				{
+					for (var i=levels.length-1; i>=0; i--)
+					{
+						if (levels[i].lineNumber <= targetLine+1)
+							return i
+					}
+					return undefined
+				}
+			)
 		}
 	}
 })
@@ -297,7 +308,7 @@ function loadText(txt)
 	editor.setValue(txt)
 	setEditorClean()
 	unloadGame()
-	compile(["restart"], txt)
+	compile(-1, txt)
 	setPageTitle()
 }
 

src/js/editor/level_editor.js
@@ -45,7 +45,17 @@ LevelEditorScreen.prototype.toggle = function()
 		{
 			if (state.title === 'EMPTY GAME')
 			{
-				compile(["loadFirstNonMessageLevel"])
+				compile(
+					function findFirstNonMessageLevel(levels)
+					{
+						for (var i=0; i<levels.length; i++)
+						{
+							if ( ! state.levels[i].hasOwnProperty("message") )
+								return i
+						}
+						return undefined
+					}
+				)
 			}
 			else
 			{

src/js/engine/engine_base.js
@@ -210,7 +210,7 @@ function tryActivateYoutube(){
 // ==========
 
 // Only called at the end of compile()
-function setGameState(_state, command = ['restart'], randomseed = null)
+function setGameState(_state, level, randomseed = null)
 {
 	oldflickscreendat=[];
 	timer=0;
@@ -225,9 +225,9 @@ function setGameState(_state, command = ['restart'], randomseed = null)
 	sfxDestroyMask=new BitVec(STRIDE_OBJ);
 
 	// show the title screen if there's no level
-	if ( ((state.levels.length === 0) || (_state.levels.length === 0) ) && (command.length > 0) && (command[0] === 'rebuild') ) // it's not that 'rebuild' is special, it's that it cannot happen in the other cases
+	if ( (level === undefined) && ( (state.levels.length === 0) || (_state.levels.length === 0) ) )
 	{
-		command = ["restart"]
+		level = -1
 	}
 	RandomGen = new RNG(randomseed)
 
@@ -253,10 +253,16 @@ function setGameState(_state, command = ['restart'], randomseed = null)
 		logWarning("throttle_movement is designed for use in conjunction with realtime_interval. Using it in other situations makes games gross and unresponsive, broadly speaking.  Please don't.");
 	}
 	
-	switch(command[0])
+	if (typeof level === 'function')
 	{
-		case 'restart':
+		level = level(state.levels)
+	}
+
+	if (level !== undefined)
+	{
+		if (level < 0)
 		{
+			// restart
 			winning=false;
 			timer=0;
 			tryPlaySimpleSound('titlescreen')
@@ -266,42 +272,10 @@ function setGameState(_state, command = ['restart'], randomseed = null)
 			menu_screen.done = false
 			menu_screen.makeTitle()
 			clearInputHistory()
-			break
 		}
-		case 'rebuild':
+		else
 		{
-			//do nothing
-			break;
-		}
-		case 'loadFirstNonMessageLevel':
-		{
-			for (var i=0; i<state.levels.length; i++)
-			{
-				if (state.levels[i].hasOwnProperty("message")){
-					continue;
-				}
-				goToLevel(state, i, randomseed)
-				break;
-			}
-			break;	
-		}
-		case 'loadLevel':
-		{
-			goToLevel(state, command[1], randomseed)
-			break;
-		}
-		case 'levelline': // called when clicking on a level line in the editor
-		{
-			const targetLine = command[1]
-			for (var i=state.levels.length-1; i>=0; i--)
-			{
-				if (state.levels[i].lineNumber <= targetLine+1)
-				{
-					goToLevel(state, i)
-					break
-				}
-			}
-			break
+			goToLevel(state, level, randomseed)
 		}
 	}
 

src/js/makegif.js
@@ -12,8 +12,8 @@ function makeGIF()
 	encoder.setDelay(200);
 	encoder.start();
 
-	compile(["loadLevel",curlevel],levelString,randomseed);
-	canvasResize();
+	compile(curlevel, levelString, randomseed)
+	canvasResize()
 
 	var gifcanvas = document.createElement('canvas');
 	const [virtual_screen_w, virtual_screen_h] = screen_layout.content.get_virtual_screen_size()

src/standalone.html
@@ -233,7 +233,7 @@ a {
 
 <script type="text/javascript">
 __JAVASCRIPT_GOES_HERE__
-compile(["restart"], __GAMEDAT__)
+compile(-1, __GAMEDAT__)
 </script>
 
 </body>

src/tests/resources/testingFrameWork.js
@@ -13,7 +13,7 @@ function runTest(dataarray) {
 	const targetlevel = dataarray[3] || 0
 	const randomseed = dataarray[4] || null
 
-	compile(["loadLevel",targetlevel], levelString, randomseed)
+	compile(targetlevel, levelString, randomseed)
 
 	if (errorStrings.length > 0)
 		return false
@@ -53,7 +53,7 @@ function runCompilationTest(game_string, recordedErrorStrings, recordedWarningSt
 	warningStrings = []
 
 	try{
-		compile(["restart"], game_string)
+		compile(-1, game_string)
 	} catch (error){
 		console.log(error)
 	}

commit ca8d68a4cef963f6654ca90576ace223739f2986

Fix a bug introduced by 3fee325b53833e9d80fa00c150197101b6f653ad
because I forgot to rewrite one call to `compile`, there would be an exception raised when rebuilding a game that has not yet been compiled.

src/js/toolbar.js
@@ -329,7 +329,7 @@ function githubLogOut()
 function rebuildClick()
 {
 	clearConsole()
-	compile(["rebuild"])
+	compile()
 	setPageTitle()
 }
 