ClementSparrow/Pattern-Script

commit d701715d2c0c150e839196b6788f491f33ba6607

replace calls to `backupLevel` with calls to `level.backUp`.

src/js/editor/level_editor.js
@@ -56,7 +56,7 @@ LevelEditorScreen.prototype.toggle = function()
 		screen_layout.content = this
 	}
 
-	restartTarget = backupLevel()
+	restartTarget = screen_layout.content.content.level.backUp()
 	canvasResize()
 }
 
@@ -295,7 +295,7 @@ LevelScreen.prototype.redraw = function(magnification)
 // TODO: the new Level function should be in a new file named editor/level.js
 Level.prototype.adjust = function(widthdelta, heightdelta)
 {
-	backups.push(backupLevel());
+	backups.push(this.backUp())
 	const oldlevel = this.clone();
 	this.width += widthdelta;
 	this.height += heightdelta;
@@ -534,7 +534,7 @@ LevelEditorScreen.prototype.levelEditorClick = function(event, click) // click i
 		if (this.anyEditsSinceMouseDown === false)
 		{
 			this.anyEditsSinceMouseDown = true
-			backups.push(backupLevel())
+			backups.push(this.content.level.backUp())
 		}
 		this.content.level.setCell(coordIndex, glyphmask)
 		redraw()

src/js/engine/engine_base.js
@@ -83,7 +83,7 @@ function loadLevelFromLevelDat(state, leveldat, randomseed)
 		screen_layout.content.level = level
 
 		backups=[]
-		restartTarget=backupLevel();
+		restartTarget = level.backUp()
 		keybuffer = []
 
 		if ('run_rules_on_level_start' in state.metadata)
@@ -145,7 +145,6 @@ Level.prototype.backUp = function()
 		oldflickscreendat: oldflickscreendat.concat([])
 	}
 }
-function backupLevel() { return level.backUp() }
 
 Level.prototype.forSerialization = function()
 {
@@ -156,7 +155,6 @@ Level.prototype.forSerialization = function()
 		oldflickscreendat: oldflickscreendat.concat([])
 	}
 }
-function level4Serialization() { return level.forSerialization() }
 
 
 // Youtube
@@ -340,7 +338,7 @@ function DoRestart(force) {
 	restarting = true;
 	if (force !== true)
 	{
-		backups.push(backupLevel());
+		backups.push(level.backUp())
 	}
 
 	if (verbose_logging) {
@@ -759,7 +757,7 @@ function processInput(dir, dontDoWin, dontModify)
 		}
 	}
 
-	var bak = backupLevel()
+	var bak = level.backUp()
 
 	var playerPositions = []
 	if (dir <= 4)
@@ -922,7 +920,7 @@ function processInput(dir, dontDoWin, dontModify)
 				{ 
 					consolePrintFromRule('CHECKPOINT command executed, saving current state to the restart state.', level.commandQueue.sourceRules[CommandsSet.command_keys.checkpoint])
 				}
-				restartTarget = level4Serialization()
+				restartTarget = level.forSerialization()
 				hasUsedCheckpoint = true
 				storage_set(document.URL+'_checkpoint', JSON.stringify(restartTarget))
 				storage_set(document.URL, curlevel)
@@ -1084,7 +1082,7 @@ function nextLevel()
 		storage_set(document.URL, curlevel)
 		if (curlevelTarget !== null)
 		{
-			restartTarget = level4Serialization()
+			restartTarget = level.forSerialization()
 			storage_set(document.URL+'_checkpoint', JSON.stringify(restartTarget))
 		} else {
 			storage_remove(document.URL+'_checkpoint')

commit c7466b6005eefaaece47b8cfd20acc17814ac003

Fix a bug introduced by d701715d2c0c150e839196b6788f491f33ba6607
When closing the level editor, it raised an exception in the console

src/js/editor/level_editor.js
@@ -38,6 +38,7 @@ LevelEditorScreen.prototype.toggle = function()
 	{
 		screen_layout.content.content.level.printToConsole()
 		screen_layout.content = screen_layout.content.content // close
+		execution_context.restartTarget = screen_layout.content.level.backUp()
 	}
 	else
 	{
@@ -50,7 +51,7 @@ LevelEditorScreen.prototype.toggle = function()
 					{
 						for (var i=0; i<levels.length; i++)
 						{
-							if ( ! state.levels[i].hasOwnProperty("message") )
+							if ( ! levels[i].hasOwnProperty('message') )
 								return i
 						}
 						return undefined
@@ -64,9 +65,8 @@ LevelEditorScreen.prototype.toggle = function()
 		}	
 		this.content = screen_layout.content // open
 		screen_layout.content = this
+		execution_context.restartTarget = this.content.level.backUp()
 	}
-
-	execution_context.restartTarget = screen_layout.content.content.level.backUp()
 	canvasResize()
 }
 