ClementSparrow/Pattern-Script

commit e59177354f8181b9ebd49cc29f6fa574171240d9

Simplify makegif

src/js/makegif.js
@@ -2,8 +2,6 @@ function makeGIF()
 {
 	var randomseed = RandomGen.seed;
 
-	var inputDat = inputHistory.concat([]);
-
 	unitTesting=true;
 	levelString=compiledText;
 
@@ -12,6 +10,8 @@ function makeGIF()
 	encoder.setDelay(200);
 	encoder.start();
 
+	// TODO: we should not have to recompile. Actually, we don't want to recompile. We want to use the replay the inputHistory in the same state (already compiled) than
+	// when we created the inputHistory.
 	compile(curlevel, levelString, randomseed)
 
 	var gifcanvas = document.createElement('canvas');
@@ -24,17 +24,22 @@ function makeGIF()
   	encoder.addFrame(gifctx);
 	var autotimer=0;
 
-  	for(const val of inputDat)
+  	for(const val of inputHistory)
   	{
   		var realtimeframe = false
-		if (val === 'undo') {
+  		switch (val)
+  		{
+		case 'undo':
 			execution_context.doUndo()
-		} else if (val === 'restart') {
-			DoRestart();
-		} else if (val === 'tick') {			
+			break
+		case 'restart':
+			DoRestart()
+			break
+		case 'tick':
 			processInput(processing_causes.autotick)
 			realtimeframe = true
-		} else {
+			break
+		default:
 			processInput(val)
 		}
 
@@ -60,7 +65,5 @@ function makeGIF()
 	consolePrint('<img class="generatedgif" src="'+data_url+'">');
 	consolePrint('<a href="'+data_url+'" download>Download GIF</a>');
   	
-  	unitTesting = false;
-
-    inputHistory = inputDat
+  	unitTesting = false
 }

commit 910543ed318f86fda617a879c7c99675385751cd

Fix GIF export in the editor that was broken by e59177354f8181b9ebd49cc29f6fa574171240d9
See also #36.

src/js/makegif.js
@@ -1,6 +1,7 @@
 function makeGIF()
 {
 	var randomseed = RandomGen.seed;
+	const inputDat = Array.from(inputHistory)
 
 	unitTesting=true;
 	levelString=compiledText;
@@ -20,11 +21,11 @@ function makeGIF()
 	gifcanvas.height = gifcanvas.style.height = virtual_screen_h * screen_layout.magnification
 	var gifctx = gifcanvas.getContext('2d');
 
-	gifctx.drawImage(canvas, -screen_layout.margins[0], -screen_layout.margins[1]);
+	gifctx.drawImage(screen_layout.canvas, -screen_layout.margins[0], -screen_layout.margins[1]);
   	encoder.addFrame(gifctx);
 	var autotimer=0;
 
-  	for(const val of inputHistory)
+	for(const val of inputDat)
   	{
   		var realtimeframe = false
   		switch (val)
@@ -44,7 +45,7 @@ function makeGIF()
 		}
 
 		redraw()
-		gifctx.drawImage(canvas, -screen_layout.margins[0], -screen_layout.margins[1])
+		gifctx.drawImage(screen_layout.canvas, -screen_layout.margins[0], -screen_layout.margins[1])
 		encoder.addFrame(gifctx)
 		encoder.setDelay(realtimeframe ? autotickinterval : repeatinterval)
 		autotimer += repeatinterval
@@ -55,7 +56,7 @@ function makeGIF()
 			redraw()
 
 			encoder.setDelay(againinterval)
-			gifctx.drawImage(canvas, -screen_layout.margins[0], -screen_layout.margins[1])
+			gifctx.drawImage(screen_layout.canvas, -screen_layout.margins[0], -screen_layout.margins[1])
 	  		encoder.addFrame(gifctx)
 		}
 	}
@@ -65,5 +66,6 @@ function makeGIF()
 	consolePrint('<img class="generatedgif" src="'+data_url+'">');
 	consolePrint('<a href="'+data_url+'" download>Download GIF</a>');
   	
-  	unitTesting = false
+	inputHistory = inputDat
+	unitTesting = false
 }