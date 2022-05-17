4d7d7c87fc425aabe88b9a2445386d6ae6598a5b
Clément Sparrow <clement.pillias@free.fr>
2022-05-17 17:00:54 +0200

    Simplify code for playing sounds

--- src/js/soundbar.js
+++ src/js/soundbar.js
@@ -3,18 +3,12 @@ function newSound(instrument)
 {
 	const seed = instrument + 100 * ((Math.random() * 1000000) | 0)
 	consolePrint(generatorNames[instrument] + ' : ' + '<span class="cm-SOUND" onclick="playSound(' + seed.toString() + ')">' + seed.toString() + '</span>',true);
-	var params = generateFromSeed(seed);
-	params.sample_rate = SAMPLE_RATE;
-	params.bit_depth = BIT_DEPTH;
-	var sound = SoundEffect.generate(params);
-	sound.play();
+	const params = generateFromSeed(seed)
+	SoundEffect.generate(params).play()
 }
 
-function buttonPress() {
-	var generatortype = 3;
-	var params = generateFromSeed( document.getElementById('sounddat').value )
-	params.sample_rate = SAMPLE_RATE;
-	params.bit_depth = BIT_DEPTH;
-	var sound = SoundEffect.generate(params);
-	sound.play();
+function buttonPress()
+{
+	const params = generateFromSeed( document.getElementById('sounddat').value )
+	SoundEffect.generate(params).play()
 }
