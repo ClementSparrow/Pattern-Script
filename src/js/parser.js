ClementSparrow/Pattern-Script

commit 2041d9b65e6bb2b4e697eba260fd0202a52f67dd

simplify the validation of meta data
at the end of the preamble

src/js/parser.js
@@ -386,43 +386,40 @@ PuzzleScriptParser.prototype.tokenInPreambleSection = function(is_start_of_line,
 	return 'METADATA';
 }
 
+PuzzleScriptParser.prototype.finalizeMetaData = function(metadata_name, default_value, error_id, validate_func)
+{
+	const key_index = this.metadata_keys.indexOf(metadata_name)
+	if (key_index < 0)
+	{
+		this.registerMetaData(metadata_name, default_value)
+		return
+	}
+
+	const value_str = this.metadata_values[key_index]
+	const value = validate_func(value_str)
+	if (value === null)
+	{
+		this.logError([error_id, value_str, default_value])
+		this.metadata_values[key_index] = default_value
+		return
+	}
+
+	this.metadata_values[key_index] = value
+}
 
 // TODO: merge with twiddleMetaData defined in compiler.js. Also, it should be done directly as we parse, not after the preamble.
 PuzzleScriptParser.prototype.finalizePreamble = function()
 {
-	const sprite_size_key_index = this.metadata_keys.indexOf('sprite_size')
-	if (sprite_size_key_index >= 0)
-	{
-		const [sprite_w, sprite_h] = this.metadata_values[sprite_size_key_index].split('x').map(s => parseInt(s))
-		if ( isNaN(sprite_w) || isNaN(sprite_h) )
+	this.finalizeMetaData('sprite_size', [5, 5], 'not_a_sprite_size',
+		function(s)
 		{
-			this.logError('Wrong parameter for sprite_size in the preamble: was expecting WxH with W and H as numbers, but got: '+this.metadata_values[sprite_size_key_index]+'. Reverting back to default 5x5 size.')
-			this.metadata_values[sprite_size_key_index] = [5, 5]
+			const result = s.split('x').map(parseInt)
+			return result.some(isNaN) ? null : result
 		}
-		else
-		{
-			this.metadata_values[sprite_size_key_index] = [sprite_w, sprite_h]
-		}
-	}
-	else
-	{
-		this.registerMetaData('sprite_size', [5, 5])
-	}
-
-	const level_title_style_index = this.metadata_keys.indexOf('level_title_style')
-	if (level_title_style_index >= 0)
-	{
-		const level_title_style = this.metadata_values[level_title_style_index]
-		if (titleStyles.indexOf(level_title_style) < 0 || level_title_style == 'default')
-		{
-			this.logError(['unknown_title_style', level_title_style])
-			this.metadata_values[level_title_style_index] = 'none'
-		}
-	}
-	else
-	{
-		this.registerMetaData('level_title_style', 'none')
-	}
+	)
+	this.finalizeMetaData('level_title_style', 'none', 'unknown_title_style',
+		s => titleStyles.includes(level_title_style) ? level_title_style : null
+	)
 }
 
 

src/editor.html
@@ -149,7 +149,7 @@ LEVELS
 	<a id="exportClickLink" title="Export a standalone HTML Build of your game." href="javascript:void('Export Standalone HTML Build');" onclick="exportClick()">EXPORT</a> - 
 	<a id="shareClickLink" title="Generate a shareable link to a playable copy of your game." href="javascript:void('Generate Link To Game');" onclick="shareClick()">SHARE</a> - 
 	<a id="switchModeClickLink" title="Switch between light/dark mode in the editor." href="javascript:void('switch editor mode');" onclick="switchLightClick()">LIGHT MODE</a> -
-	<a href="Documentation/documentation.html" title="How Pattern:Script Works."  target="Puzzle_Script_Documentation">DOCS</a>
+	<a href="Documentation/documentation.html" title="How Pattern:Script Works."  target="Pattern_Script_Documentation">DOCS</a>
 </div>
 
 <div id="righttophalf">	

src/js/editor/error_messages.js
@@ -54,6 +54,7 @@ const error_messages = { // actually also warning messages
 
 	// prelude:
 	unknown_metadata: 'Unrecognised stuff in the prelude.',
+	not_a_sprite_size: (value_str, default_value) => 'Wrong parameter for sprite_size in the preamble: was expecting WxH with W and H as numbers, but got: ' + value_str + '. Reverting back to default ' + default_value + ' size.',
 	// objects:
 	palette_too_small: (i,n,l) => 'Trying to access color number ' + i + ' from the color palette of sprite ' + n + ', but there are only ' + l + ' defined in it.',
 	// sounds:
@@ -66,7 +67,7 @@ const error_messages = { // actually also warning messages
 	long_level_title: 'Long level title might get truncated on pause menu.',
 	repeated_level_number: 'You\'ve already specified a number for this level. Overriding the previous number.',
 	repeated_level_title: 'You\'ve already specified a title for this level. Overriding the previous title.',
-	unknown_title_style: style => 'Unknown title style "' + style + '". Refer to the documentation for possible values.',
+	unknown_title_style: (style, default_style) => 'Unknown title style "' + style + '". Reverting to default "' + default_style + '". Refer to <a href="../Documentation/prelude.html" target="Pattern_Script_Documentation">the documentation</a> for possible values.',
 	// rule_parser.js
 	// ==============
 

commit fc9b8fde5f6286f32ee5100ad067cb4dca45a8bf

fix bug with parsing `sprite_size`
The bug was introduced in 2041d9b65e6bb2b4e697eba260fd0202a52f67dd.
stupid javascript.

src/js/parser.js
@@ -419,7 +419,7 @@ PuzzleScriptParser.prototype.finalizePreamble = function()
 	this.finalizeMetaData('sprite_size', [5, 5], 'not_a_sprite_size',
 		function(s)
 		{
-			const result = s.split('x').map(parseInt)
+			const result = s.split('x').map(str => parseInt(str))
 			return result.some(isNaN) ? null : result
 		}
 	)