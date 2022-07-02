ae96fe8bf1f214c6b3aed1ba10a9af48021d081a
Clément Sparrow <clement.pillias@free.fr>
2022-07-02 17:00:36 +0200

    fix a critical bug with empty messages in levels

--- src/js/compiler.js
+++ src/js/compiler.js
@@ -334,7 +334,7 @@ function levelsToArray(state)
 		{
 			for (let message of message_box)
 			{
-				message.text = wordwrapAndColor(message.text, state.fgcolor)
+				message.text = (message.text.length > 0) ? wordwrapAndColor(message.text, state.fgcolor) : []
 				if (message.text.length >= terminal_height)
 				{
 					logWarning('Message too long to fit on screen.', message.lineNumber)
