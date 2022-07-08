0068efce03210805597568df92566115fe06e49c
Martin Ender <m.buettner.d@gmail.com>
2022-07-08 22:18:32 +0200

    Don't ignore titles for title:none style

--- src/js/parser.js
+++ src/js/parser.js
@@ -1584,7 +1584,7 @@ PuzzleScriptParser.prototype.setLevelTitle = function(title_text, title_style)
 
 	let current_level = this.levels[this.levels.length-1]
 	current_level.title_style = title_style
-	if ( (title_text == '') || (title_style == 'none') )
+	if ( (title_text == '') )
 		return
 	current_level.title = title_text
 }
